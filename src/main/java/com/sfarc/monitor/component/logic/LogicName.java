3f2ac2fccc08d66c793a044e7bf2c9e27b841363
tharuash <tharindushane002@gmail.com>
2021-04-04 01:11:16 +0530

    Logic factory added.

---
+++ src/main/java/com/sfarc/monitor/component/logic/LogicName.java
@@ -0,0 +1,10 @@
+package com.sfarc.monitor.component.logic;
+
+/**
+ * @author Tharindu Aththnayake
+ * @since 04/04/2021 12:51 AM
+ */
+public enum LogicName
+{
+	TEMP_LOGIC, HUMID_LOGIC, PRESSURE_LOGIC
+}
