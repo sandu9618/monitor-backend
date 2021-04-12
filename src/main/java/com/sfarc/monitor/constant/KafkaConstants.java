ea11147a52337bf75a98911ba9a8e798b5a919fd
sandu9618 <sandunipavithra9611@gmail.com>
2021-04-12 17:39:24 +0530

    Added kafka configurations

---
+++ src/main/java/com/sfarc/monitor/constant/KafkaConstants.java
@@ -0,0 +1,15 @@
+package com.sfarc.monitor.constant;
+
+/**
+ * @author Sanduni Pavithra
+ * Created on 4/12/2021
+ */
+public class KafkaConstants {
+
+    public static final String WEBSOCKET_DESTINATION = "/topic/messages";
+
+    public static final String KAFKA_TOPIC = "messages";
+
+    public KafkaConstants() {
+    }
+}
